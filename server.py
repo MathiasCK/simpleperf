f94769551ebae719c96d03e0870cfde2ca6713e7
mathiasck <mathias.ck2000@gmail.com>
2023-03-15 11:53:06 +0100

    Make server multi threaded

--- server.py
+++ server.py
@@ -1,27 +1,43 @@
 from utils import utils
-from socket import socket, AF_INET, SOCK_STREAM
+from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
+from threading import Thread, Lock
 
-SERVER_SOCK = socket(AF_INET, SOCK_STREAM)
+server = socket(AF_INET, SOCK_STREAM)
 
-def handleRequest(sock):
-    print("handleRequest()")
+clients = set()
+clients_lock = Lock()
+
+def handleRequest(client, addr, bind, port):
+    print(f"A simpleperf client <{addr[0]}:{addr[1]}> is connected with <{bind}:{port}>")
 
 def Main():
     bind, port, format = utils.checkServerOpts()
     try:
-        SERVER_SOCK.bind((bind, int(port)))
+        server.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+        server.bind((bind, int(port)))
         print("---------------------------------------------")
         print(f"A simpleperf server is listening on port {str(port)}")
         print("---------------------------------------------")
-        SERVER_SOCK.listen(1)
+        server.listen(1)
     except Exception as err:
         raise Exception(f"Bind failed: {repr(err)}")
     
-    sock, addr = SERVER_SOCK.accept()
+    try:
+        while True:
+            client, addr = server.accept()
 
-    handleRequest(sock)
-
-    SERVER_SOCK.close()
+            with clients_lock:
+                clients.add(client)
+    
+            thread = Thread(
+                target=handleRequest, args=(client, addr, bind, port))
+            thread.start()
+    except KeyboardInterrupt:
+        print("Stopped by Ctrl+C")
+    finally:
+        # Close server connection
+        if server:
+            server.close()
 
 if __name__ == "__main__":
     Main()
