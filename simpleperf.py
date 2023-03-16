1ed9518c36dbc6b857da13e84915d0d6840b1eb4
mathiasck <mathias.ck2000@gmail.com>
2023-03-16 15:46:03 +0100

    Simpleperf.py: add comments

--- simpleperf.py
+++ simpleperf.py
@@ -2,9 +2,12 @@ import client, server
 from utils import utils
 
 def Main():
+    # If server flag is included (-s or --server) run as server
     if utils.checkMode() == "server":
         return server.Main()
+    # Else run as client
     client.Main()
     
+# Code execution starts here
 if __name__ == "__main__":
     Main()
